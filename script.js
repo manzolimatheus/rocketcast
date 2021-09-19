852ec4124f827c5c25835688482940871650786e
Matheus Manzoli <manzoli.contato@gmail.com>
2021-09-19 15:28:55 -0300

    v01

---
+++ script.js
@@ -0,0 +1,10 @@
+function menu_mobile(){
+    const lista = document.getElementById('ul_mobile').style
+
+    if (lista.display === 'block'){
+        lista.display = 'none'
+    }else{
+        lista.display = 'block'
+    }
+    
+}
